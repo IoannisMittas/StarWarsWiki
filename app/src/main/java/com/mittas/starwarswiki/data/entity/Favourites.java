c305d008bdee614d136056138125c7e11b2327a4
Yannis <mittas.ioannis@gmail.com>
2018-06-23 14:29:59 +0300

    filled Favourites

--- app/src/main/java/com/mittas/starwarswiki/data/entity/Favourites.java
+++ app/src/main/java/com/mittas/starwarswiki/data/entity/Favourites.java
@@ -1,7 +1,38 @@
 package com.mittas.starwarswiki.data.entity;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
 
-@Entity
+import static android.arch.persistence.room.ForeignKey.CASCADE;
+
+@Entity (foreignKeys = @ForeignKey(entity = People.class,
+        parentColumns = "id",
+        childColumns = "peopleId",
+        onDelete = CASCADE))
 public class Favourites {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private int peopleId;
+
+    public Favourites(int peopleId) {
+        this.peopleId = peopleId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPeopleId() {
+        return peopleId;
+    }
+
+    public void setPeopleId(int peopleId) {
+        this.peopleId = peopleId;
+    }
 }
