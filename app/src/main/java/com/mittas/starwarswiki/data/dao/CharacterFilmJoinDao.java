65339d2f6457a4a03da92b1a82c929df9a19ceeb
Yannis <mittas.ioannis@gmail.com>
2018-06-23 17:19:03 +0300

    implemented join Daos

--- app/src/main/java/com/mittas/starwarswiki/data/dao/CharacterFilmJoinDao.java
+++ app/src/main/java/com/mittas/starwarswiki/data/dao/CharacterFilmJoinDao.java
@@ -1,7 +1,27 @@
 package com.mittas.starwarswiki.data.dao;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.mittas.starwarswiki.data.entity.CharacterFilmJoin;
+import com.mittas.starwarswiki.data.entity.Film;
+
+import java.util.List;
 
 @Dao
 public interface CharacterFilmJoinDao {
+    @Insert
+    void insert(CharacterFilmJoin characterFilmJoin);
+
+    @Query("SELECT * FROM character INNER JOIN character_film_join "
+            + "ON character.id = character_film_join.characterId "
+            + "WHERE character_film_join.filmId = :filmId")
+    LiveData<List<Character>> getCharactersByFilmId(final int filmId);
+
+    @Query("SELECT * FROM film INNER JOIN character_film_join "
+            + "ON film.id = character_film_join.filmId "
+            + "WHERE character_film_join.characterId = :characterId")
+            LiveData<List<Film>> getFilmsByCharacterId(final int characterId);
 }
--- app/src/main/java/com/mittas/starwarswiki/data/dao/CharacterVehicleJoinDao.java
+++ app/src/main/java/com/mittas/starwarswiki/data/dao/CharacterVehicleJoinDao.java
@@ -1,4 +1,28 @@
 package com.mittas.starwarswiki.data.dao;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.mittas.starwarswiki.data.entity.CharacterVehicleJoin;
+import com.mittas.starwarswiki.data.entity.Vehicle;
+
+
+import java.util.List;
+
+@Dao
 public interface CharacterVehicleJoinDao {
+    @Insert
+    void insert(CharacterVehicleJoin characterVehicleJoin);
+
+    @Query("SELECT * FROM character INNER JOIN character_vehicle_join "
+            + "ON character.id = character_vehicle_join.characterId "
+            + "WHERE character_vehicle_join.vehicleId = :vehicleId")
+    LiveData<List<Character>> getCharactersByVehicleId(final int vehicleId);
+
+    @Query("SELECT * FROM vehicle INNER JOIN character_vehicle_join "
+            + "ON vehicle.id = character_vehicle_join.vehicleId "
+            + "WHERE character_vehicle_join.characterId = :characterId")
+    LiveData<List<Vehicle>> getVehiclesByCharacterId(final int characterId);
 }
